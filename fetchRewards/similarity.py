0c43c507f7f7fc73a8f06e9cfce77a1f4478402d
Essen007 <jhu41@illinois.edu>
2021-04-29 00:08:48 -0700

    Add files via upload

--- fetchRewards/similarity.py
+++ fetchRewards/similarity.py
@@ -12,10 +12,11 @@ def compute_wer(text1="",text2=""):
     for i in range(1,l1 + 1):
         for j in range(1,l2+1):
             if t1[i-1] == t2[j-1]:
-                dp2[j] = min(dp1[j] + 1, dp1[j - 1], dp2[j - 1] + 1)
+                dp2[j] = dp1[j - 1]
             else:
                 dp2[j] = min(dp1[j - 1], dp1[j], dp2[j - 1]) + 1
         dp1 = dp2[:]
+        dp2 = [i + 1 for _ in range(l2 + 1)]
         result = dp1[-1]
 
     return 100 - (result / l2) * 100
